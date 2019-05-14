b37a63f7febf17fda238c5bee42a8599a647121d
wubh2012 <wubh2012@163.com>
2019-05-14 18:09:42 +0800

    1. 使用 tween.js 改进滚动的用户体验 2. 修复某些情况下 navbar 没有 sticky 的 bug

--- main.js
+++ main.js
@@ -19,9 +19,15 @@ var loadingAnimation = function () {
   }, 1000);
 }
 
+function animate(time) {
+  requestAnimationFrame(animate);
+  TWEEN.update(time);
+}
+requestAnimationFrame(animate);
+
 var enableScrollPosition = function () {
   let aTags = document.querySelectorAll('.topNavBar nav>ul>li>a');
-  log(aTags)
+  // log(aTags)
   for (let index = 0; index < aTags.length; index++) {
     const element = aTags[index];
     element.addEventListener('click', function (event) {
@@ -34,33 +40,52 @@ var enableScrollPosition = function () {
         var targetY = document.querySelector(href).offsetTop - 80;
         //window.scroll(0, targetY - 80);
 
-        // 1s播放24帧，肉眼不容易识别
-        let n = 25;
-        let duration = 1000 / n; // 多少时间执行
-        let currentY = window.scrollY;
-        let distance = (targetY - currentY) / n; //每次移动的距离
-        let i = 0;
-        let intervalId = setInterval(() => {
-          if (i === n) {
-            clearInterval(intervalId)
-            return;
-          }
-          i++;
-          window.scroll(0, currentY + distance * i);
-        }, duration);
+        // 1s播放24帧以上肉眼不容易识别 24fps
+        let n = 25; // 每秒播放的次数
+        let duration = 500 / n; // 每次播放的时间间隔
+        let currentY = window.scrollY; // 当前文档已经滚动的像素
+        
+        // let distance = (targetY - currentY) / n; //每次移动的距离
+        // let i = 0;
+        // let intervalId = setInterval(() => {
+        //   if (i === n) {
+        //     clearInterval(intervalId)
+        //     return;
+        //   }
+        //   i++;
+        //   window.scroll(0, currentY + distance * i);
+        // }, duration);
+
+        // 使用 tween 实现滚动动画
+        var coords = {
+          y: currentY
+        };
+
+        var tween = new TWEEN.Tween(coords) // Create a new tween that modifies 'coords'.
+        .to({y: targetY }, 1000) // Move to (200) in 1 second.
+        .easing(TWEEN.Easing.Quadratic.Out) // Use an easing function to make the animation smooth.
+        .onUpdate(function() { // Called after tween.js updates 'coords'.
+          window.scroll(0, coords.y);            
+        })
+        .start(); // Start the tween immediately.
       }
     })
   }
 }
 
-var autoStickNavBar = function () {
+
+var autoStickNavbar = function () {
   var topNavBar = document.querySelector('.topNavBar');
-  window.onscroll = function (xxx) {
+  var stickNavbar = function(){
     if (window.scrollY > 0) {
       topNavBar.classList.add('sticky');
     } else {
-      topNavBar.classList.remove('sticky');
+      topNavBar.classList.remove('sticky');      
     }
+  }
+  stickNavbar();
+  window.onscroll = function (xxx) {
+    stickNavbar()
   }
 }
 
@@ -76,28 +101,19 @@ var displaySubmenu = function () {
   var liTags = document.querySelectorAll('.menu > li');
   for (let index = 0; index < liTags.length; index++) {
     const element = liTags[index];
-    element.addEventListener('mouseenter', function (event) {
-      //log(event.target)
-      // log(event.currentTarget)
-      var currentElement = event.currentTarget
-      //log(currentNode.nextSibling.nodeType, Node.TEXT_NODE)
-      //var submenu = currentNode.querySelectorAll('ul')[0]
-      currentElement.classList.add('active')
-
+    element.addEventListener('mouseenter', function (event) {      
+      event.currentTarget.classList.add('active')
     });
     element.addEventListener('mouseleave', function (event) {
-      var currentElement = event.currentTarget
-      //var submenu = currentNode.querySelectorAll('ul')[0]
-      currentElement.classList.remove('active')
+      event.currentTarget.classList.remove('active')
     })
   }
 }
 
 
-
 var __main = function () {
-  loadingAnimation();
-  autoStickNavBar();
+  loadingAnimation();  
+  autoStickNavbar();
   displaySubmenu();
   enableScrollPosition();
   portfolioEvent();
